ae8a97c180b77729247be60381137e096bcb1aa6
yurickas <48949427+yurickas@users.noreply.github.com>
2024-10-19 18:45:42 +0300

    Merge pull request #1
    * COD-5: add infrastructure

---
+++ helper/string_helper/string_to_uuid.go
@@ -0,0 +1,12 @@
+package string_helper
+
+import (
+	"github.com/google/uuid"
+	"strings"
+)
+
+func StringToUUID(text string) (uuid.UUID, error) {
+	id := strings.Trim(text, "/")
+
+	return uuid.Parse(id)
+}
